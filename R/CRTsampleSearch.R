3f9c11e885c3cc6ff5a0a8eb7dd8c85d7e7904c6
Ruoshui Zhai <ruoshui_zhai@brown.edu>
2019-10-16 14:10:14 -0400

    update replications in simPower

--- R/CRTsampleSearch.R
+++ R/CRTsampleSearch.R
@@ -196,20 +196,20 @@ CRTsearch = function(nrep=1e4, nt, nc, tcRatio=1, minpower=0.8, alpha=0.05, incr
 #' @export
 simPower = function(nrep=1e4, nt, nc, alpha=0.05, FUN_TestStat, uppersided=NULL, ...){
   mcCores = parallel::detectCores()
-  if(nrep>1e3){
-    TH0THa = TH0THa_i = NULL
-    for(nrep_i in 1:ceiling(nrep/1e3)){
-      rm(TH0THa_i)
-      gc()  # free up memory before forking
-      TH0THa_i = parallel::mclapply(1:nrep, TestStat_TH0THa, nt=nt, nc=nc, FUN_TestStat=FUN_TestStat, ..., mc.cores=mcCores-1)
-      TH0THa_i = plyr::ldply(TH0THa_i, data.frame)
-      TH0THa = rbind(TH0THa, TH0THa_i)
-    }
-  }else{
-    gc()  # free up memory before forking
+  ##if(nrep>1e3){
+  ##  TH0THa = TH0THa_i = NULL
+  ##  for(nrep_i in 1:ceiling(nrep/1e3)){
+  ##    rm(TH0THa_i)
+  ##    gc()  # free up memory before forking
+  ##    TH0THa_i = parallel::mclapply(1:nrep, TestStat_TH0THa, nt=nt, nc=nc, FUN_TestStat=FUN_TestStat, ..., mc.cores=mcCores-1)
+  ##    TH0THa_i = plyr::ldply(TH0THa_i, data.frame)
+  ##    TH0THa = rbind(TH0THa, TH0THa_i)
+  ##  }
+  ##}else{
+  ##  gc()  # free up memory before forking
     TH0THa = parallel::mclapply(1:nrep, TestStat_TH0THa, nt=nt, nc=nc, FUN_TestStat=FUN_TestStat, ..., mc.cores=mcCores-1)
     TH0THa = plyr::ldply(TH0THa, data.frame)
-  }
+  ##}
   power = simPowerTH0Ha(TH0=TH0THa$TH0, THa=TH0THa$THa, alpha=alpha, ...)
   return(power)
 }  
